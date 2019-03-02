ff669b4af47028fe5f59f32620668f380f66a190
AMELIALADRON <amelia_97@lie.com.mx>
2019-03-01 21:06:27 -0600

    se cambio el texto del tv

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Para cambiar el texto de tv
-        tv.setText("Hola mundo");
+        tv.setText("Amelia Ladron de Guevara Martinez");
 
         //Actividades
         //a) Modifica el texto "Hola mundo", pon tu nombre y ejecuta la aplicacion
